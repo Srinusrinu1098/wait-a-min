baa5c5cfc73526b4867382369d7e7b67d4c4f752
srinu <tsrinivasulu1098@gmail.com>
2024-10-17 15:22:25 +0000

    still

--- src/components/TeamMatches/index.js
+++ src/components/TeamMatches/index.js
@@ -1,6 +1,9 @@
 import {Component} from 'react'
+import './index.css'
 
 class TeamMatches extends Component {
+  state = {backGround: '', banner: ''}
+
   componentDidMount = () => {
     this.getTeamById()
   }
@@ -11,11 +14,28 @@ class TeamMatches extends Component {
     const {id} = params
     const response = await fetch(`https://apis.ccbp.in/ipl/${id}`)
     const data = await response.json()
-    console.log(data.latest_match_details)
+    console.log(data)
+    const formatedBanner = {
+      bannerUrl: data.team_banner_url,
+    }
+
+    this.setState({backGround: id, banner: formatedBanner.bannerUrl})
   }
 
   render() {
-    return <h1>hi</h1>
+    const {backGround, banner} = this.state
+    console.log(banner)
+    console.log(backGround)
+    return (
+      <div className={`${backGround}`}>
+        <div className="banner">
+          <img src={banner} alt="team banner" className="team-image" />
+        </div>
+        <div className="display">
+          <p className="main-para">Latest Matches</p>
+        </div>
+      </div>
+    )
   }
 }
 
