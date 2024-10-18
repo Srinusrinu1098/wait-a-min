681fbb0b6d4fe2ead58329d11d4ce58ae66a5a23
srinu <tsrinivasulu1098@gmail.com>
2024-10-18 11:26:56 +0000

    will doooo

--- src/components/LatestMatch/index.js
+++ src/components/LatestMatch/index.js
@@ -1,1 +1,46 @@
-// Write your code here
+import './index.css'
+
+const LatestMatch = props => {
+  const {value} = props
+  const {
+    competingTeam,
+    competingTeamLogo,
+    date,
+    firstInnings,
+    id,
+    manOfTheMatch,
+    results,
+    secondInnings,
+    umpires,
+    venue,
+  } = value
+
+  return (
+    <li key={id} className="li-back-ground">
+      <div>
+        <h1 className="latest-heading3">{competingTeam}</h1>
+        <p className="latest-heading4">{date}</p>
+        <p className="latest-heading2">{venue}</p>
+        <p className="latest-heading2">{results}</p>
+      </div>
+      <div>
+        <img
+          src={competingTeamLogo}
+          alt={competingTeam}
+          className="latest-img"
+        />
+      </div>
+      <div>
+        <h1 className="latest-heading">First Innings</h1>
+        <p className="latest-heading1">{firstInnings}</p>
+        <h1 className="latest-heading">Second Innings</h1>
+        <p className="latest-heading1">{secondInnings}</p>
+        <h1 className="latest-heading">Man Of The Match</h1>
+        <p className="latest-heading1">{manOfTheMatch}</p>
+        <h1 className="latest-heading">Umpires</h1>
+        <p className="latest-heading1">{umpires}</p>
+      </div>
+    </li>
+  )
+}
+export default LatestMatch
--- src/components/TeamMatches/index.js
+++ src/components/TeamMatches/index.js
@@ -1,8 +1,9 @@
 import {Component} from 'react'
+import LatestMatch from '../LatestMatch'
 import './index.css'
 
 class TeamMatches extends Component {
-  state = {backGround: '', banner: ''}
+  state = {backGround: '', banner: '', recentMatches: {}}
 
   componentDidMount = () => {
     this.getTeamById()
@@ -19,11 +20,30 @@ class TeamMatches extends Component {
       bannerUrl: data.team_banner_url,
     }
 
-    this.setState({backGround: id, banner: formatedBanner.bannerUrl})
+    const recent = data.latest_match_details
+
+    const formatedRecent = {
+      competingTeam: recent.competing_team,
+      competingTeamLogo: recent.competing_team_logo,
+      date: recent.date,
+      firstInnings: recent.first_innings,
+      id: recent.id,
+      manOfTheMatch: recent.man_of_the_match,
+      results: recent.result,
+      secondInnings: recent.second_innings,
+      umpires: recent.umpires,
+      venue: recent.venue,
+    }
+
+    this.setState({
+      backGround: id,
+      banner: formatedBanner.bannerUrl,
+      recentMatches: formatedRecent,
+    })
   }
 
   render() {
-    const {backGround, banner} = this.state
+    const {backGround, banner, recentMatches} = this.state
     console.log(banner)
     console.log(backGround)
     return (
@@ -34,6 +54,7 @@ class TeamMatches extends Component {
         <div className="display">
           <p className="main-para">Latest Matches</p>
         </div>
+        <LatestMatch value={recentMatches} />
       </div>
     )
   }
